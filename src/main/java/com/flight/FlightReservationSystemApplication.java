3ba6969dfae6ea8e114bf2e12047d2a263c67bb4
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2026-02-23 15:50:52 +0530

    Added some changes

--- src/main/java/com/flight/FlightReservationSystemApplication.java
+++ src/main/java/com/flight/FlightReservationSystemApplication.java
@@ -3,7 +3,6 @@ package com.flight;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
